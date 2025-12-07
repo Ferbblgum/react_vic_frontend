a72d6728e4d7cb291f421c2800ab54868e7f95c4
Fernanda <158494236+Ferbblgum@users.noreply.github.com>
2025-12-06 23:06:48 -0600

    On branch main

---
+++ react_vic_frontend/src/Components/DataProvider/Api.js
@@ -0,0 +1,49 @@
+class Api {
+  constructor(baseUrl, headers) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _handserveresponse(res) {
+    if (!res.ok) {
+      throw new Error(Error `: ${res.status}`);
+    }
+    return res.json();
+  }
+  
+
+  updateCard(cardId,like){
+    return fetch(`${this._baseUrl}/cards/${cardId}`,{
+      method:"PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        like
+      })
+    }).then(this._handserveresponse)
+  }
+
+  createCard(data) {
+  return fetch(`${this._baseUrl}/cards`, {
+    method: "POST",
+    headers: this._headers,
+    body: JSON.stringify(data),
+  }).then(this._handserveresponse);
+}
+  deleteCard(cardId){
+    return fetch(`${this._baseUrl}/cards/${cardId}`,{
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handserveresponse)
+  }
+  getAllcards() {
+    return fetch(`${this._baseUrl}/getAllCards`, {
+      headers: this._headers,
+    }).then(this._handserveresponse);
+  }
+}
+
+const api = new Api("https://exercise3-vic.onrender.com", {
+  "Content-Type": "application/json",
+});
+
+export default api;
